d6ae5f4cae21234beed7df075739610685aa3fba
GotoFinal <bartlomiejkmazur@gmail.com>
2020-05-24 19:07:50 +0200

    Improved name generator

--- Assets/GotoUdon/PlayerTemplate.cs
+++ Assets/GotoUdon/PlayerTemplate.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
 
 namespace GotoUdon.Editor
 {
     [System.Serializable]
     public class PlayerTemplate
     {
-        private static int _nextNumber = 1;
+        private static readonly Random _random = new System.Random();
+
+        private static readonly string[] nameGeneratorWords =
+        {
+            "Goto", "Final", "The", "Ant", "Super", "Dog", "Cat", "Like", "Loli", "Hate", "Mark", "Eddie", "Steve", "Blue", "Black",
+            "White", "Yellow", "Red", "Green", "Pro", "Cookie", "Lover", "I", "Love", "Fox", "Foxes", "Cats", "Dogs", "Cookies",
+            ""
+        };
 
         public string playerName;
         public GameObject avatarPrefab;
@@ -17,12 +26,36 @@ namespace GotoUdon.Editor
 
         public static PlayerTemplate CreateNewPlayer(bool firstOne)
         {
+            HashSet<string> usedNamed = new HashSet<string>();
+            foreach (PlayerTemplate instancePlayerTemplate in GotoUdonSettings.Instance.playerTemplates)
+            {
+                usedNamed.Add(instancePlayerTemplate.playerName);
+            }
+
+            string newName = GenerateName();
+            while (usedNamed.Contains(newName))
+            {
+                newName += _random.Next(0, 10);
+            }
+
             return new PlayerTemplate
             {
-                playerName = "GotoFinal_" + _nextNumber++,
+                playerName = newName,
                 joinByDefault = firstOne,
                 customId = -1
             };
+        }
+
+        private static string GenerateName()
+        {
+            int length = _random.Next(1, 6);
+            string name = "";
+            for (int i = 0; i < length; i++)
+            {
+                name += nameGeneratorWords[_random.Next(nameGeneratorWords.Length)];
+            }
+
+            return name;
         }
     }
 }
