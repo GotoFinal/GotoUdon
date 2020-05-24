98c93b9e4bd16f7810377e872bedd065b0f0b082
GotoFinal <bartlomiejkmazur@gmail.com>
2020-05-24 20:16:55 +0200

    Bump version

--- Assets/GotoUdon/Editor/GotoUdonEditor.cs
+++ Assets/GotoUdon/Editor/GotoUdonEditor.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 public class GotoUdonEditor : EditorWindow
 {
-    public const string VERSION = "v1.1.3";
+    public const string VERSION = "v1.2.0";
     public const string ImplementedSDKVersion = "2020.05.06.12.14";
     public static string CurrentSDKVersion => VRC.Core.SDKClientUtilities.GetSDKVersionDate();
 
